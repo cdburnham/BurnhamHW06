5fe53cc0f8cc913e6f39bb5bdf6dd6a8bf32b711
cdburnham <camdburnham@icloud.com>
2025-03-22 20:43:08 -0400

    Update myCopier.cpp

--- Problem2/myCopier.cpp
+++ Problem2/myCopier.cpp
@@ -1,1 +1,53 @@
+/* Created by: Cameron Burnham
+ *
+ * The purpose of this program is to copy the contents of one text file (arg 1)  
+to another text file (arg2).
+ *
+ * Invokation: ./myCopier "file1.txt" "file2.txt"
+ *
+ * ... as long as files referenced are within the program's directory.
+ * */
 
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+            // Verify correct argument quantity...
+            if (argc != 3) {
+                    cerr << "Usage is argv[0] <source_txt> <destination_txt>" <<<
+ endl;
+                   return 1;
+            }
+
+           string sourceFile = argv[1];
+           string destinationFile = argv[2];
+
+           ifstream inputFile(sourceFile);
+           if (!inputFile){
+                cerr << "Error: couldn't open the source file..." << sourceFile  
+<< " to copy from." << endl;
+                return 1;
+           }
+
+           ofstream outputFile(destinationFile);
+           if (!outputFile){
+                cerr << "Error: couldn't access the destination file..." << destt
+inationFile << " to copy to." << endl;
+                return 1;
+           }
+
+           string line;
+           while (getline(inputFile, line)){
+                outputFile << line << endl;
+           }
+
+           inputFile.close();
+           outputFile.close();
+
+           cout << "File copied from: " << sourceFile << " to: " << destinationFF
+ile << "succesfully!" << endl;
+           return 0;
+}
